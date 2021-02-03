365f27d4ceaf2bba577a2c1e40da3b0c3827f3be
amossennis <amossenn@ucsd.edu>
2021-02-03 09:34:39 -0800

    Slight change

--- vxus-rep/src/app/app.module.ts
+++ vxus-rep/src/app/app.module.ts
@@ -9,15 +9,15 @@ import { FooterComponent } from './components/footer/footer.component';
 import { SubscriptionComponent } from './components/subscription/subscription.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VxusComponent } from './components/vxus/vxus.component';
-import { GroupedbarchartComponent } from './groupedbarchart/groupedbarchart.component';
+import { GroupedbarchartComponent } from './components/groupedbarchart/groupedbarchart.component';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatPaginatorModule} from '@angular/material/paginator';
-import { LinebarchartComponent } from './linebarchart/linebarchart.component';
-import { LinechartComponent } from './linechart/linechart.component';
+import { LinebarchartComponent } from './components/linebarchart/linebarchart.component';
+import { LinechartComponent } from './components/linechart/linechart.component';
 @NgModule({
   declarations: [
     AppComponent,
